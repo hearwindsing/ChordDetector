75b5e41c3d566df9156ba26a47e9f3e5f7f09e36
hearwindsing <shevchenko.anastasiy@gmail.com>
2016-03-15 00:27:02 +0300

    change main

--- Main.py
+++ Main.py
@@ -9,35 +9,31 @@ from numpy import mean
 import csv
 import os
 
-chroma = []
-chords = []
-
 def exportCollection(file):
+    chroma = []
+    chords = []
     with open(file, 'rb') as csvfile:
         f = csv.reader(csvfile, delimiter=',')
         for row in f:
-            if (row!='---')and(len(row)!=1):
-                A = []
-                for i in range(1,25):
-                   A.append(float(row[i]))
-                chroma.append(A)
-                if len(row) == 26:
-                    chords.append(row[25])
-                else:
-                    chords.append(row[25]+','+row[26])
+            chrm = map(float, row[2:])
+            chroma.append(chrm)
+            chords.append(row[0])
+    return (chords, chroma)
 
-exportCollection('chroma.txt')
-print chords
-                                                                                                                                                                                                                                                                                                        
-print('Ya tut')
-X = chroma
-y = chords
-lenChroma = len(chroma)
-print lenChroma
-kf = KFold(lenChroma, n_folds = 10)
-model = GaussianNB()
-print(model)
 
-scores = sklearn.cross_validation.cross_val_score(estimator=model, X=X, y=y, cv=kf, scoring="accuracy")
-score = mean(scores)
-print score
+if __name__ == '__main__':
+    print "Load collection"
+    (chords, chroma) = exportCollection('chroma.txt')
+
+    print len(chroma)
+
+    X = chroma
+    y = chords
+    lenChroma = len(X)
+
+    kf = KFold(lenChroma, n_folds=10)
+    model = GaussianNB()
+
+    scores = sklearn.cross_validation.cross_val_score(estimator=model, X=X, y=y, cv=kf, scoring="accuracy")
+    score = mean(scores)
+    print score
